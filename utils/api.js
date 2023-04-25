a80ab1eff28c48a148382edb248ac66a20477c07
gabriel silva <gabriel.gs605@gmail.com>
2023-04-24 22:38:46 -0300

    second commit

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-import { STRAPI_API_TOKEN, API_URL } from './urls';
+import { API_URL, STRAPI_API_TOKEN } from './urls';
 
 export const fetchDataFromApi = async (endpoint) => {
   const options = {
@@ -7,8 +7,22 @@ export const fetchDataFromApi = async (endpoint) => {
       Authorization: 'Bearer ' + STRAPI_API_TOKEN,
     },
   };
+
   const res = await fetch(`${API_URL}${endpoint}`, options);
   const data = await res.json();
 
+  return data;
+};
+
+export const makePaymentRequest = async (endpoint, payload) => {
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + STRAPI_API_TOKEN,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+  const data = await res.json();
   return data;
 };
